134ad1d58afbf47330dc9094538465220ca7ab5a
devkwonms <alstj1379@nate.com>
2023-10-13 18:59:15 +0900

    react-router-dom V6에 맞게 코드 수정

--- frontend/src/app/AppRoutes.js
+++ frontend/src/app/AppRoutes.js
@@ -1,63 +1,49 @@
-import React, { Component,Suspense, lazy } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import Spinner from '../app/shared/Spinner';
 
-// const Home = lazy(() => import('./Home'));
 const Dashboard = lazy(() => import('./dashboard/Dashboard'));
-
 const Buttons = lazy(() => import('./basic-ui/Buttons'));
 const Dropdowns = lazy(() => import('./basic-ui/Dropdowns'));
 const Typography = lazy(() => import('./basic-ui/Typography'));
 
+
+
+
 const BasicElements = lazy(() => import('./form-elements/BasicElements'));
-
 const BasicTable = lazy(() => import('./tables/BasicTable'));
-
 const Mdi = lazy(() => import('./icons/Mdi'));
-
 const ChartJs = lazy(() => import('./charts/ChartJs'));
-
 const Error404 = lazy(() => import('./error-pages/Error404'));
 const Error500 = lazy(() => import('./error-pages/Error500'));
-
 const Login = lazy(() => import('./user-pages/Login'));
 const Register1 = lazy(() => import('./user-pages/Register'));
 
+function AppRoutes() {
+  const navigate = useNavigate();
 
-class AppRoutes extends Component {
-  render () {
-    return (
-      <Suspense fallback={<Spinner/>}>
-        <Switch>
-          {/* <Route exact path="/home" component={Home} /> */}
-          <Route exact path="/dashboard" component={ Dashboard } />
-
-          <Route path="/basic-ui/buttons" component={ Buttons } />
-          <Route path="/basic-ui/dropdowns" component={ Dropdowns } />
-          <Route path="/basic-ui/typography" component={ Typography } />
-
-          <Route path="/form-Elements/basic-elements" component={ BasicElements } />
-
-          <Route path="/tables/basic-table" component={ BasicTable } />
-
-          <Route path="/icons/mdi" component={ Mdi } />
-
-          <Route path="/charts/chart-js" component={ ChartJs } />
-
-
-          <Route path="/user-pages/login-1" component={ Login } />
-          <Route path="/user-pages/register-1" component={ Register1 } />
-
-          <Route path="/error-pages/error-404" component={ Error404 } />
-          <Route path="/error-pages/error-500" component={ Error500 } />
-
-
-          <Redirect to="/dashboard" />
-        </Switch>
-      </Suspense>
-    );
-  }
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        {/* Uncomment this if you have a Home component */}
+        {/* <Route path="/home" element={<Home />} /> */}
+        <Route path="/dashboard" element={<Dashboard()} />
+        <Route path="/basic-ui/buttons" element={<Buttons />} />
+        <Route path="/basic-ui/dropdowns" element={<Dropdowns />} />
+        <Route path="/basic-ui/typography" element={<Typography />} />
+        <Route path="/form-Elements/basic-elements" element={<BasicElements />} />
+        <Route path="/tables/basic-table" element={<BasicTable />} />
+        <Route path="/icons/mdi" element={<Mdi />} />
+        <Route path="/charts/chart-js" element={<ChartJs />} />
+        <Route path="/user-pages/login-1" element={<Login />} />
+        <Route path="/user-pages/register-1" element={<Register1 />} />
+        <Route path="/error-pages/error-404" element={<Error404 />} />
+        <Route path="/error-pages/error-500" element={<Error500 />} />
+        <Route path="*" element={() => navigate('/dashboard')} />
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default AppRoutes;
