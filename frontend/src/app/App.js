5f597bd994ba2871ce53a6e7e61e7fe990650ccb
devkwonms <alstj1379@nate.com>
2023-10-15 17:49:56 +0900

    version up refactoring

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,73 +1,61 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, Outlet } from 'react-router-dom';
 import './App.scss';
-import AppRoutes from './AppRoutes';
 import Navbar from './shared/Navbar';
 import Sidebar from './shared/Sidebar';
 import Footer from './shared/Footer';
-import { withTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-class App extends Component {
-  state = {}
-  componentDidMount() {
-    this.onRouteChanged();
-  }
-  render () {
-    let navbarComponent = !this.state.isFullPageLayout ? <Navbar/> : '';
-    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar/> : '';
-    let footerComponent = !this.state.isFullPageLayout ? <Footer/> : '';
-    return (
-      <div className="container-scroller">
-        { sidebarComponent }
-        <div className="container-fluid page-body-wrapper">
-          { navbarComponent }
-          <div className="main-panel">
-            <div className="content-wrapper">
-              <AppRoutes/>
-            </div>
-            { footerComponent }
-          </div>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  const location = useLocation();
+  const { i18n } = useTranslation();
+  const [isFullPageLayout, setIsFullPageLayout] = useState(false);
 
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      this.onRouteChanged();
-    }
-  }
+  useEffect(() => {
+    onRouteChanged();
+  }, [location]);
 
-  onRouteChanged() {
-    console.log("ROUTE CHANGED");
-    const { i18n } = this.props;
+  const onRouteChanged = () => {
+    console.log('ROUTE CHANGED');
     const body = document.querySelector('body');
-    if(this.props.location.pathname === '/layout/RtlLayout') {
+    if (location.pathname === '/layout/RtlLayout') {
       body.classList.add('rtl');
       i18n.changeLanguage('ar');
-    }
-    else {
-      body.classList.remove('rtl')
+    } else {
+      body.classList.remove('rtl');
       i18n.changeLanguage('en');
     }
-    window.scrollTo(0, 0);
-    const fullPageLayoutRoutes = ['/user-pages/login-1', '/user-pages/login-2', '/user-pages/register-1', '/user-pages/register-2', '/user-pages/lockscreen', '/error-pages/error-404', '/error-pages/error-500', '/general-pages/landing-page'];
-    for ( let i = 0; i < fullPageLayoutRoutes.length; i++ ) {
-      if (this.props.location.pathname === fullPageLayoutRoutes[i]) {
-        this.setState({
-          isFullPageLayout: true
-        })
-        document.querySelector('.page-body-wrapper').classList.add('full-page-wrapper');
-        break;
-      } else {
-        this.setState({
-          isFullPageLayout: false
-        })
-        document.querySelector('.page-body-wrapper').classList.remove('full-page-wrapper');
-      }
-    }
-  }
 
+    window.scrollTo(0, 0);
+
+    const fullPageLayoutRoutes = [
+      '/user-pages/login-1',
+      '/user-pages/login-2',
+      '/user-pages/register-1',
+      '/user-pages/register-2',
+      '/user-pages/lockscreen',
+      '/error-pages/error-404',
+      '/error-pages/error-500',
+      '/general-pages/landing-page',
+    ];
+
+    setIsFullPageLayout(fullPageLayoutRoutes.includes(location.pathname));
+  };
+
+  return (
+    <div className="container-scroller">
+      {!isFullPageLayout && <Sidebar />}
+      <div className="container-fluid page-body-wrapper">
+        {!isFullPageLayout && <Navbar />}
+        <div className="main-panel">
+          <div className="content-wrapper">
+            <Outlet />
+          </div>
+          {!isFullPageLayout && <Footer />}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default withTranslation()(withRouter(App));
+export default App;
