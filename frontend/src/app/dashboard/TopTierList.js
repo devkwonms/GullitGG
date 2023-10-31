03684a66614be2afbfc0b2e98a00627312df5d14
devkwonms <alstj1379@nate.com>
2023-10-31 23:36:57 +0900

    add TopTierList.js

---
+++ frontend/src/app/dashboard/TopTierList.js
@@ -0,0 +1,19 @@
+import { Box, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+
+function TopTierList({ matchType, division, achievementDate, divisionImgUrl }) {
+  return (
+    <Box>
+      <img src={divisionImgUrl}></img>
+      <Typography>{matchType}</Typography>
+      <Typography>{division}</Typography>
+      <Typography>{achievementDate}</Typography>
+    </Box>
+  );
+}
+TopTierList.propTypes = {
+  matchType: PropTypes.string.isRequired,
+  division: PropTypes.string.isRequired,
+  achievementDate: PropTypes.string.isRequired,
+};
+export default TopTierList;
