dbfb1ff593a6602c5c7a05218ec508b5367f3545
devkwonms <alstj1379@nate.com>
2023-11-12 22:02:36 +0900

    MatchTypeButton

--- frontend/src/app/dashboard/Dashboard.js
+++ frontend/src/app/dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import { Doughnut } from "react-chartjs-2";
 import Slider from "react-slick";
 import { TodoListComponent } from "../apps/TodoList";
 import { VectorMap } from "react-jvectormap";
-import useDidMountEffect from "../customhook/useDidMountEffect";
+import { Box, Tab, Tabs } from "@mui/material";
 import axios from "axios";
 
 const mapData = {
@@ -71,6 +71,12 @@ function Dashboard() {
 
     fetchData();
   }, [matchType, offset]);
+
+  const MatchTypeButton = ({ matchType, onClick }) => (
+    <button className="btn btn-primary" onClick={() => onClick(matchType)}>
+      {matchType === 50 ? "공식경기" : matchType === 52 ? "감독모드" : " "}
+    </button>
+  );
 
   const handleLoadMore = () => {
     setOffset(offset + 10); // increase the number of matches to display by 10
@@ -168,11 +174,11 @@ function Dashboard() {
         <div className="col-sm-4 grid-margin">
           <div className="card">
             <div className="card-body">
-              <h5>공식경기 최고티어</h5>
+              <h5>감독모드 최고티어</h5>
               <div className="row">
                 <div className="col-8 col-sm-12 col-xl-8 my-auto">
                   <div className="d-flex d-sm-block d-md-flex align-items-center">
-                    <h2 className="mb-0">공식경기</h2>
+                    <h2 className="mb-0">감독모드</h2>
                     <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
                   </div>
                   <h6 className="text-muted font-weight-normal">11.38% Since last month</h6>
@@ -188,76 +194,17 @@ function Dashboard() {
         <div className="col-sm-4 grid-margin">
           <div className="card">
             <div className="card-body">
-              <h5>감독모드 최고티어</h5>
+              <h5>볼타모드 최고티어</h5>
               <div className="row">
                 <div className="col-8 col-sm-12 col-xl-8 my-auto">
                   <div className="d-flex d-sm-block d-md-flex align-items-center">
-                    <h2 className="mb-0">공식경기</h2>
+                    <h2 className="mb-0">볼타모드</h2>
                     <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
                   </div>
                   <h6 className="text-muted font-weight-normal">11.38% Since last month</h6>
                 </div>
                 <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
                   <i className="icon-lg mdi mdi-codepen text-primary ml-auto"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-4 grid-margin">
-          <div className="card">
-            <div className="card-body">
-              <h5>Revenue</h5>
-              <div className="row">
-                <div className="col-8 col-sm-12 col-xl-8 my-auto">
-                  <div className="d-flex d-sm-block d-md-flex align-items-center">
-                    <h2 className="mb-0">공식경기</h2>
-                    <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
-                  </div>
-                  <h6 className="text-muted font-weight-normal">11.38% Since last month</h6>
-                </div>
-                <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                  <i className="icon-lg mdi mdi-codepen text-primary ml-auto"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-4 grid-margin">
-          <div className="card">
-            <div className="card-body">
-              <h5>Sales</h5>
-              <div className="row">
-                <div className="col-8 col-sm-12 col-xl-8 my-auto">
-                  <div className="d-flex d-sm-block d-md-flex align-items-center">
-                    <h2 className="mb-0">감독모드</h2>
-                    <p className="text-success ml-2 mb-0 font-weight-medium">+8.3%</p>
-                  </div>
-                  <h6 className="text-muted font-weight-normal"> 9.61% Since last month</h6>
-                </div>
-                <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                  <i className="icon-lg mdi mdi-wallet-travel text-danger ml-auto"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-4 grid-margin">
-          <div className="card">
-            <div className="card-body">
-              <h5>Purchase</h5>
-              <div className="row">
-                <div className="col-8 col-sm-12 col-xl-8 my-auto">
-                  <div className="d-flex d-sm-block d-md-flex align-items-center">
-                    <h2 className="mb-0">친선경기</h2>
-                    <p className="text-danger ml-2 mb-0 font-weight-medium">-2.1% </p>
-                  </div>
-                  <h6 className="text-muted font-weight-normal">2.27% Since last month</h6>
-                </div>
-                <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                  <i className="icon-lg mdi mdi-monitor text-success ml-auto"></i>
                 </div>
               </div>
             </div>
